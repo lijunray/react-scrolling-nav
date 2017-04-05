4441aa23d9e33464458a3b589dd19e113d83dc4d
raylee <lijunray@me.com>
2017-04-05 19:55:38 -0400

    change bundle file to non-minified

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,37 @@
 var path = require('path');
 
-module.exports = function (env) {
-    return {
-        entry: env.production ? './src/navbar/index.js' : './src/index.js',
-        output: {
-            path: path.join(__dirname, env.production ? 'lib/' : 'static/'),
-            filename: env.production ? 'index.js' : 'app.bundle.js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.jsx?$/,
-                    loader: 'babel-loader',
-                    exclude: /node_modules/,
-                    options: {
-                        presets: ['react', 'es2015']
-                    }
-                },
-                {
-                    test: /\.(less|css)$/,
-                    use: [
-                        'style-loader',
-                        'css-loader',
-                        'less-loader'
-                    ]
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'static/'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['react', 'es2015']
                 }
-            ]
-        },
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: 1000
-        },
-        devServer: {
-            historyApiFallback: true,
-            contentBase: './static/'
-        }
-    };
+            },
+            {
+                test: /\.(less|css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './static/'
+    }
 };
